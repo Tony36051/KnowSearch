275a0c7c57f517a0c13047394266253c7aa74cfe
w00406273 <w00406273@huawei.com>
2026-06-09 11:03:07 +0800

    历史内容都不要，从头来

--- crx/entrypoints/background.ts
+++
@@ -1,8 +0,0 @@
-export default defineBackground(() => {
-  // Modify the storage item in the background to demonstrate that the composable works
-  setInterval(async () => {
-    const oldValue = await storage.getItem<number>("session:count");
-    const newValue = (oldValue ?? 0) + 1;
-    await storage.setItem("session:count", newValue);
-  }, 1000);
-});
